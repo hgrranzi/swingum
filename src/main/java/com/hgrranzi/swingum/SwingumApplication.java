ac76e2949f2b5644b4ba294238fc095fb6118554
hgrranzi <vkh.mariia@gmail.com>
2024-04-10 22:57:02 +0200

    swingum - jdbc driver dependency added

--- src/main/java/com/hgrranzi/swingum/SwingumApplication.java
+++ src/main/java/com/hgrranzi/swingum/SwingumApplication.java
@@ -6,9 +6,20 @@ import com.hgrranzi.swingum.view.console.ConsoleFrame;
 import com.hgrranzi.swingum.view.gui.GuiFrame;
 
 import javax.swing.*;
+import java.sql.Connection;
+
+import static java.sql.DriverManager.getConnection;
 
 public class SwingumApplication {
     public static void main(String[] args) {
+        try (Connection connection = getConnection("jdbc:postgresql://localhost:5432/swingum",
+                                                   "postgres",
+                                                   "postgres")) {
+            System.out.println("Database connection established");
+        } catch (Exception e) {
+            System.out.println("Database connection failed: " + e.getMessage());
+            //todo: if no database connection, use file storage or do not load/save game
+        }
         UserInterface ui = args.length > 0 && args[0].equals("console") ? new ConsoleFrame() : new GuiFrame();
         SwingUtilities.invokeLater(() -> {
             ViewController controller = new ViewController(ui);
